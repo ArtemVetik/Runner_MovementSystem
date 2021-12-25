e5867a1528e99536b5b26c05ab6ba4bb1b3c9361
Artem <av.unitygames@gmail.com>
2021-12-25 19:06:41 +0300

    Create PathConnector for easy connection of segments

---
+++ Assets/RunnerMovementSystem/Scripts/Editor/PathConnectorEditor.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+using RunnerMovementSystem;
+
+namespace RunnerMovementEditor
+{
+    [CustomEditor(typeof(PathConnector))]
+    public class PathConnectorEditor : Editor
+    {
+        private PathConnector _pathConnector;
+
+        private void Awake()
+        {
+            _pathConnector = target as PathConnector;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Connect Start"))
+                _pathConnector.SetupStartPosition();
+            if (GUILayout.Button("Connect End"))
+                _pathConnector.SetupEndPosition();
+
+            if (_pathConnector.StartConnected == false)
+                EditorGUILayout.HelpBox("Start point is not connected. Push \"Connect Start\" button.", MessageType.Warning);
+            if (_pathConnector.EndConnected == false)
+                EditorGUILayout.HelpBox("End point is not connected. Push \"Connect End\" button.", MessageType.Warning);
+
+            base.OnInspectorGUI();
+        }
+    }
+}
